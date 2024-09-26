c018fc5e044625e725f2a9031f7f1ebd20c537e2
Adalberto <adalberto.nota11@gmail.com>
2024-09-26 13:54:47 -0300

    CREATE BUILD

--- src/http/server.ts
+++ src/http/server.ts
@@ -17,7 +17,7 @@ app.register(getSingleChapter);
 
 app.listen({
     host: "0.0.0.0",
-    port: 3333
+    port: process.env.PORT ? Number(process.env.PORT) : 3333
 }).then(() => {
     console.log("Server HTTP running!");
 })
